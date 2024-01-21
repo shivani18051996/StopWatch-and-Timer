7d0f4dac40eb07e064678adea238bd66251c710b
shivani18051996 <shivanitrivedi17@gmail.com>
2024-01-21 12:19:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import StopWatch from "./components/stopwatch/StopWatch";
+import Timer from "./components/timer/Timer";
+import Header from "./components/common/header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <div className="container">
+        <Routes>
+          <Route path="/" exact element={<StopWatch/>} />
+          <Route path="/timer" element={<Timer/>} />
+        </Routes>
+      </div>
+    </Router>
+  </div>
   );
 }
 
